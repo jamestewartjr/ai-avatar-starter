699f04c3312a4c8ff5e8ed46566df47f39049586
A <>
2023-01-16 22:48:09 -0500

    complete minting

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useEffect,useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { ethers } from "ethers";
+import minter from '../utils/minter.json'
+
+const CONTRACT_ADDRESS = "0xF7170CD3750818b8EcE45670152E76D1eD147092";
 
 const Home = () => {
   const maxRetries = 5;
@@ -12,7 +15,8 @@ const Home = () => {
   const [retryCount, setRetryCount] = useState(maxRetries);
   const [isGenerating, setIsGenerating] = useState(false);
   const [finalPrompt, setFinalPrompt] = useState('');
-
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [transactInfo, setTransactionInfo] = useState("");
 
   const onChange = (event) => {
     setInput(event.target.value);
@@ -66,6 +70,38 @@ const Home = () => {
     setIsGenerating(false);
   }
 
+  const checkIfWalletIsConnected = async () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
+  
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  
+    /*
+      * Check if we're authorized to access the user's wallet
+      */
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    /*
+      * User can have multiple authorized accounts, we grab the first one if its there!
+      */
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener()
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
   useEffect(() => {
     const runRetry = async () => {
       if (retryCount === 0) {
@@ -88,10 +124,112 @@ const Home = () => {
     runRetry();
   }, [retry]);
 
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, minter.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("SquaredMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, minter.abi, signer);
+  
+        console.log("Going to pop wallet now to pay gas...")
+        let nftTxn = await connectedContract.mintNFT();
+  
+        console.log("Mining...please wait.")
+        await nftTxn.wait();
+        console.log(nftTxn);
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+        setTransactionInfo(`https://goerli.etherscan.io/tx/${nftTxn.hash}`)
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="generate-button">
+      Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="generate-button">
+      Mint NFT
+    </button>
+  )
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator </title>
       </Head>
       <div className="container">
         <div className="header">
@@ -121,6 +259,15 @@ const Home = () => {
               </div>
             </a>
           </div>
+          <div className="header-subtitle">
+            <p className="sub-text">
+              Each unique. Each beautiful. Discover your NFT today.
+            </p>
+            {currentAccount === "" ? renderNotConnectedContainer(): renderMintUI()}
+          </div>
+        </div>
+        <div className="header-subtitle">
+          Your NFT tranasaction info: {transactInfo}
         </div>
         {img && (
           <div className="output-content">
@@ -128,18 +275,6 @@ const Home = () => {
             <p>{finalPrompt}</p>
           </div>
         )}
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
       </div>
     </div>
   );
